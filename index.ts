37dea349666ca90ed70b42f1ca1475e09d9cff30
Raghav <raghavsekhri2k1@gmail.com>
2024-03-07 17:51:46 +0530

    added cors

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import { identifyRoutes } from "./routes/identifyRoutes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/", identifyRoutes);
